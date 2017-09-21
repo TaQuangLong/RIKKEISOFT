3a3393e565e18513c2fb34a1e132a098a87633a7
long <longtq0807@gmail.com>
2017-09-21 22:52:13 +0700

    commit changed

--- app/src/main/java/tq42tlu/quanglong/rikkeisoft/MainActivity.java
+++ app/src/main/java/tq42tlu/quanglong/rikkeisoft/MainActivity.java
@@ -2,12 +2,27 @@ package tq42tlu.quanglong.rikkeisoft;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvName, tvSlogan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvName=(TextView)findViewById(R.id.textviewName);
+
+        tvName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"WHERE THE DREAM BEGIN",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
